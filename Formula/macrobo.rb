b8d8e4c8a3a00bc7e3217d3b1c1f4049903f8aba
oaquique <30644106+oaquique@users.noreply.github.com>
2026-02-19 15:58:27 -0800

    Update macrobo to v1.0.22

--- Formula/macrobo.rb
+++ Formula/macrobo.rb
@@ -1,9 +1,9 @@
 class Macrobo < Formula
   desc "Fast, multi-threaded file replication tool for macOS"
   homepage "https://github.com/oaquique/macrobo"
-  url "https://github.com/oaquique/macrobo/releases/download/v1.0.21/macrobo-1.0.21-universal.tar.gz"
-  sha256 "e04c730877e9be2fef9b0f2df9194f4dbf23f28464abb55e55b6659ca0f8b409"
-  version "1.0.21"
+  url "https://github.com/oaquique/macrobo/releases/download/v1.0.22/macrobo-1.0.22-universal.tar.gz"
+  sha256 "9d30b6bddff71e83004892ef640b521edb4a244da7200ef36a1cfe2671c931af"
+  version "1.0.22"
   license "MIT"
 
   depends_on :macos
